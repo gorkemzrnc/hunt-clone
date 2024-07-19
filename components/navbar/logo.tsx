00649f311122269a209c6fd45605595cb1062fc9
gorkemzrnc <gorkemzurnaci@hotmail.com>
2024-07-19 12:21:45 +0300

    initial

---
+++ components/navbar/logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Logo = () => {
+  return (
+    <div>
+      <Link href={"/"} className="md:hidden">
+        
+      </Link>
+    </div>
+  )
+}
+
+export default Logo
